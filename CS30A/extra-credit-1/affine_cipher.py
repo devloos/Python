0fc4098bd6261aee9fa281c8fdeb3269026e3ebf
Carlos Aguilera <18aguilerac@gmail.com>
2023-12-05 22:12:20 -0800

    update

--- CS30A/extra-credit-1/affine_cipher.py
+++ CS30A/extra-credit-1/affine_cipher.py
@@ -1,13 +1,15 @@
-key_alpha = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7,  'h': 8, 'i': 9,  'j': 10, 'k': 11,  'l': 12, 'm': 13,
-             'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22,  'w': 23, 'x': 24, 'y': 25,  'z': 26, ' ': 27}
+from egcd import extended_gcd
 
-key_digit = dict(map(reversed, key_alpha.items()))
+KEY_ALPHA = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7,  'h': 8, 'i': 9,  'j': 10, 'k': 11,  'l': 12, 'm': 13,
+             'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22,  'w': 23, 'x': 24, 'y': 25,  'z': 26, ' ': 27}
+KEY_DIGIT = dict(map(reversed, KEY_ALPHA.items()))
+ENCODED_COUNT = 27
 
 
 def digits_to_string(digits: [int]) -> str:
     message = ''
     for digit in digits:
-        message += key_digit[digit]
+        message += KEY_DIGIT[digit]
 
     return message
 
@@ -16,12 +18,12 @@ def encrypt(message: str, a: int, b: int) -> str:
     encrypted_message = ''
     digits = []
     for c in message:
-        i = key_alpha[c.lower()]
+        i = KEY_ALPHA[c.lower()]
 
-        mod = (a * i + b) % 27
+        mod = (a * i + b) % ENCODED_COUNT
 
         if (mod == 0):
-            mod = 27
+            mod = ENCODED_COUNT
 
         digits.append(mod)
 
@@ -30,16 +32,23 @@ def encrypt(message: str, a: int, b: int) -> str:
     return encrypted_message
 
 
+def mod_inverse(a, m) -> int:
+    _, coefs = extended_gcd(a, m)
+
+    return coefs[0] % m
+
+
 def decrypt(message: str, a, b) -> str:
+    a_inverse = mod_inverse(a, ENCODED_COUNT)
     decrypted_message = ''
     digits = []
     for c in message:
-        i = key_alpha[c.lower()]
+        i = KEY_ALPHA[c.lower()]
 
-        res = int((a ** -1) * i - (a ** -1) * b) % 27
+        res = (a_inverse * (i - b)) % ENCODED_COUNT
 
         if (res == 0):
-            res = 27
+            res = ENCODED_COUNT
 
         digits.append(res)
 
